d3588e690e1fd705f3044972e22a2901f5039226
Chingyen <124688629+chingyen06@users.noreply.github.com>
2025-03-28 19:05:29 +0800

    Add files via upload

---
+++ string_with_space/string_with_space.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char str[100];  //字元陣列當字串
+
+    //fgets(字串, 字串大小, 輸入)
+    fgets(str, 100, stdin);  //輸入可包含空格的字串
+
+    //strcspn(字串, "要找的字串")  找到引號 "" 中的字串第一個字所在的索引
+    str[strcspn(str, "\n")] = '\0';  //將換行符號改為字串結束符號
+
+    printf("%s", str);
+}
